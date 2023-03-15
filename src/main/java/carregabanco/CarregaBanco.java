d3441f4ded3bc854f3e0a6c7004e58b0dfe6425a
unknown <laboratoriodm@laboratorio.ifms.br>
2023-03-14 20:33:39 -0400

    criação da aplicação desktop(criação banco)

---
+++ src/main/java/carregabanco/CarregaBanco.java
@@ -0,0 +1,10 @@
+package carregabanco;
+
+import carregabanco.view.CarregaBancoView;
+
+public class CarregaBanco {
+	public static void main(String[] args) {
+		new CarregaBancoView();
+		//AlunoModel alunoModel = AlunoDao.getInstance().getById(1);    
+	}	
+}
