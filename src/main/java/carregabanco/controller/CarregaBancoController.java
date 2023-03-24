052ad2c8ac5ef557ff6c06301dc49e2bf79fd7c8
Alexandre Mendonça <lexgol@live.com>
2023-03-23 22:12:35 -0400

    Continuação metodo inserirNoBanco corrigido

--- src/main/java/carregabanco/controller/CarregaBancoController.java
+++ src/main/java/carregabanco/controller/CarregaBancoController.java
@@ -15,78 +15,12 @@ import carregabanco.repository.AlunoDao;
 
 public class CarregaBancoController {
 
-	public static void main(String[] args) throws SQLException {
-
-		Scanner scanner = new Scanner(System.in);
-		int opcao;
-
-		do {
-			System.out.println("Selecione a opção desejada:");
-			System.out.println("1 - Cadastrar ");
-			System.out.println("2 - Editar ");
-			System.out.println("3 - Buscar ");
-			System.out.println("4 - Listar ");
-			System.out.println("5 - Excluir");
-
-			System.out.println("0 - Sair");
-
-			opcao = scanner.nextInt();
-			scanner.nextLine();
-
-			switch (opcao) {
-			case 1:
-				inserirNoBanco(scanner);
-				break;
-			/*case 2:
-				update(scanner);
-				break;
-			case 3:
-				buscar();
-				break;
-			case 4:
-				listar();
-				break;
-			case 5:
-				excluir();
-				break;*/
-			case 0:
-				System.out.println("Até a próxima!");
-				break;
-			default:
-				System.out.println("Opção inválida.");
-			}
-		} while (opcao != 0);
-	}
-
-	public static void inserirNoBanco(Scanner scanner) {
-		
-		System.out.println("######################### Atualizar #########################");
-
-		System.out.println("Digite o nome do aluno ");
-		String nome_estudante = scanner.nextLine();
-		System.out.println("Digite a cordenação ");
-		String coordenacao = scanner.next();
-		System.out.println("Digite o curso ");
-		String curso = scanner.next();
-		System.out.println("Digite a situação ");
-		String situacao = scanner.next();
-		System.out.println("Digite a o período de entrada ");
-		String periodo_entrada = scanner.next();
-		
-		AlunoModel aluno = new AlunoModel();
-		aluno.setNome_estudante(nome_estudante);
-		aluno.setCoordenacao(coordenacao);
-		aluno.setCurso(curso);
-		aluno.setSituacao(situacao);
-		aluno.setPeriodo_entrada(periodo_entrada);
-		
-		AlunoDao alunoDao = new AlunoDao();
+	public void inserirNoBanco (AlunoModel aluno) {
+		AlunoDao alunoDao = AlunoDao.getInstance();
 		alunoDao.persist(aluno);
-		
-		System.out.println("Aluno cadastro com sucesso");
-		
-
 	}
+		
+		
 
 	private void update(Scanner scanner) {
 
--- src/main/java/carregabanco/view/CarregaBancoView.java
+++ src/main/java/carregabanco/view/CarregaBancoView.java
@@ -1,27 +1,28 @@
 package carregabanco.view;
 
-import java.util.ArrayList;
 import carregabanco.controller.CarregaBancoController;
-import carregabanco.model.AlunoModel;
 
 public class CarregaBancoView {
-	
+
 	public CarregaBancoView() {
-		
+
 		CarregaBancoController carregaBancoController = new CarregaBancoController();
-		ArrayList<AlunoModel> valores = carregaBancoController.inserirNoBanco(aluno);
-		carregaBancoController.inserirNoBanco(valores);
-						
-		//A parte da visualiza��o seria isso
-		System.out.println(valores.size());
-		for(AlunoModel n: valores)
-		System.out.println(n.getCampus() + " - " + n.getNome_estudante()+ " - " + n.getIdade());
 		
-		
+		/*ArrayList<AlunoModel> valores = carregaBancoController.loader();
+		 carregaBancoController.loader("alunostads.csv");
+		 * //carregaBancoController.inserirNoBanco(valores);
+		 * 
+		 * //A parte da visualiza��o seria isso //System.out.println(valores.size());
+		 * for(AlunoModel n: valores) System.out.println(n.getCampus() + " - " +
+		 * n.getNome_estudante()+ " - " + n.getIdade());
+		 * 
+		 * 
+		 * }
+		 */
 	}
+
 	public void menu() {
 		
+
 	}
-	
-	
 }
